800698bd50fd99171ea9c52e517869bb41ec4c74
nchiorsac <nchiorsac@student.unimelb.edu.au>
2023-03-29 20:28:06 +1100

    implemented handling of command line input

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    char* filename;
+    char* scheduler;
+    char* memory_strategy;
+    int quantum;
+
+    // Process command line arguments
+    if (argc<5){
+        printf("Incorrect number of arguments\n");
+        exit(1);
+    }
+
+    filename = argv[1];
+    scheduler = argv[2];
+    memory_strategy = argv[3];
+    quantum = atoi(argv[4]);
+
     return 0;
 }
